e25b0b3d054a99bfe6b63328345dd3a677635ee7
Kseniya Yakil <k.yakil@yandex.ru>
2016-04-26 10:00:22 +0300

    - store texts as json - remove texts if there are <= 2 sentences - add stop words

--- text_preprocess/process.py
+++ text_preprocess/process.py
@@ -22,12 +22,15 @@ def main():
 				return
 
 		text_p = TextProcess(batch_size=10, debug=opt.debug)
+		out_fname = "res.json"
 
 		#text_p.process_from_file("text_from_db.txt")
 		#text_p.store_into_file("text_form_db.txt")
-		texts = text_p.preprocess(1, 100)
+		texts = text_p.preprocess(1, -1)
 		print "************"
 		text_p.get_fixed_word_len(texts, 1, 3)
+		print "Storing to " + out_fname
+		text_p.store_as_json(texts, out_fname)
 
 if __name__ == '__main__':
 		main()
